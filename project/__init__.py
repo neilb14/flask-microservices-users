7193718b97df25a235eb88a9f3c995a3c864cda7
Neil Bourgeois <neil.bourgeois@gmail.com>
2017-08-09 23:05:47 -0600

    get app config section from env vars

--- project/__init__.py
+++ project/__init__.py
@@ -1,7 +1,8 @@
 from flask import Flask, jsonify
 
 app = Flask(__name__)
-app.config.from_object('project.config.DevelopmentConfig')
+app_settings = os.getenv('APP_SETTINGS')
+app.config.from_object(app_settings)
 
 @app.route('/ping', methods=['GET'])
 def ping_pong():
